ea125120a76d44c0f63c11c8c22eac734d0046dc
Amelia Aronsohn <squirrel@wearing.black>
2023-02-15 10:13:32 -0800

    SOme tools need python

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
