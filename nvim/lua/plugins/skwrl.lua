a2b4458e2f4a8cffbecdb90cc70a865580572a4b
Amelia Aronsohn <squirrel@wearing.black>
2023-04-04 12:07:40 -0700

    Use mini-surround, clean up personal plugins

--- nvim/lua/plugins/skwrl.lua
+++ nvim/lua/plugins/skwrl.lua
@@ -1,29 +1,18 @@
 -- Quck lang plugins. maybe refactor later
 return {
-  "danihodovic/vim-ansible-vault",
-  "LokiChaos/vim-tintin",
-  "dougireton/vim-chef",
-  "rizzatti/dash.vim",
+  "christoomey/vim-tmux-navigator",
 
   "tpope/vim-eunuch", -- First class unix commands
-
   "direnv/direnv.vim",
-
-  "junegunn/vim-easy-align",
-
-  "christoomey/vim-tmux-navigator",
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  },
 
   -- the gits
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
+
+  "junegunn/vim-easy-align",
+
+  "danihodovic/vim-ansible-vault",
+  "LokiChaos/vim-tintin",
+  "dougireton/vim-chef",
+  "rizzatti/dash.vim",
 }
