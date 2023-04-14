1dd62e811ec75b11d64014638bde74d332f7971b
Amelia Aronsohn <squirrel@wearing.black>
2023-04-14 12:07:27 -0700

    Drop f-keys, move to whichkey leader
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,15 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- The F-Keys
--- Toggle line numbers
-vim.keymap.set("n", "<F2>", ":set number!<CR>", { desc = "Toggle Line Numbers" })
--- Toggle Tree
-vim.keymap.set("n", "<F3>", ":Neotree<CR>")
 -- cd to the current file
-vim.keymap.set("n", "<F4>", ":cd %:p:h<CR>:pwd<CR>", { desc = "cd to curret file dir" })
--- rename
-vim.keymap.set("n", "<F6>", vim.lsp.buf.rename)
+vim.keymap.set("n", "<Leader>p", ":cd %:p:h<CR>:pwd<CR>", { desc = "cd to current file dir" })
 
 -- Quick open a bottom terminal for commands
 vim.keymap.set("n", "<Leader>o", ":botright 20split +term<CR>i", { desc = "Quick Terminal" })
