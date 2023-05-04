b2a6dd4b3ed09533fb98fcf052c2f0ba5e9dd4be
Amelia Aronsohn <squirrel@wearing.black>
2023-05-04 08:17:47 -0700

    make go a thing for now
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -45,7 +45,6 @@ return {
         "black",
         "rust-analyzer",
         "stylua",
-        "shellcheck",
       },
     },
   },
@@ -101,5 +100,20 @@ return {
     init = function()
       require("crates").show()
     end,
+  },
+
+  -- Golang
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
