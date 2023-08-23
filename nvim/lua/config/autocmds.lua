f5d59c03d4617887bb87ec49f28d563ea0f47315
Amelia Aronsohn <squirrel@wearing.black>
2023-08-23 16:31:11 -0700

    Disable yaml autofmt

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -20,6 +20,16 @@ ft("*.omnifocusjs", "javascript")
 ft("*.omnioutlinerjs", "javascript")
 
 --
+-- Disable autoformat for yaml files
+--
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "yaml" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+--
 -- General Autocmds
 --
 
