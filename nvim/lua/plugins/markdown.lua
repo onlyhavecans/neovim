0ac0c0c16b6d38341912bc20bed7c412065a1c02
Amelia Aronsohn <squirrel@wearing.black>
2024-02-15 11:58:30 -0800

    feat: optimize markdown headlines plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,4 +1,22 @@
--- I am a smidge picky with markdown
 return {
-  { "lukas-reineke/headlines.nvim", opts = { markdown = { fat_headlines = false } } },
+  -- The header color scheme from Obsidian
+  vim.cmd([[
+    highlight Headline1 gui=bold guifg=#D52D00
+    highlight Headline2 gui=bold guifg=#EF7627
+    highlight Headline3 gui=bold guifg=#FF9A56
+    highlight Headline4 gui=bold guifg=#D162A4
+    highlight Headline5 gui=bold guifg=#B55690
+    highlight Headline6 gui=bold guifg=#A30262
+  ]]),
+
+  -- I don't like when plugins mutate underlying characters
+  {
+    "lukas-reineke/headlines.nvim",
+    opts = {
+      markdown = {
+        fat_headlines = false,
+        bullets = {},
+      },
+    },
+  },
 }
