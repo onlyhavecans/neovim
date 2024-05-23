8ccce66642210ca9475e99ef66484058ae6e78d7
Amelia Aronsohn <squirrel@wearing.black>
2024-05-22 18:09:40 -0700

    feat: drop mason

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -12,4 +12,6 @@ return {
 
   -- Disabled Defaults
   { "folke/noice.nvim", enabled = false }, -- Noice popups are a no for me
+  { "williamboman/mason.nvim", enabled = false }, -- Not compatible with Nix
+  { "williamboman/mason-lspconfig.nvim", enabled = false }, -- Not compatible with Nix
 }
