ea427068d71ca6ef6bf28fcd029625e37bc124fc
Amelia Aronsohn <squirrel@wearing.black>
2024-07-24 07:56:39 -0700

    refactor: headlines is replaced with markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,12 +1,1 @@
 vim.opt.linebreak = true
-
--- The header color scheme from Obsidian
--- Works with "lukas-reineke/headlines.nvim",
-vim.cmd([[
-  highlight Headline1 gui=bold guifg=#D52D00
-  highlight Headline2 gui=bold guifg=#EF7627
-  highlight Headline3 gui=bold guifg=#FF9A56
-  highlight Headline4 gui=bold guifg=#D162A4
-  highlight Headline5 gui=bold guifg=#B55690
-  highlight Headline6 gui=bold guifg=#A30262
-]])
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,12 +1,1 @@
-return {
-  -- I don't like when plugins mutate underlying characters
-  {
-    "lukas-reineke/headlines.nvim",
-    opts = {
-      markdown = {
-        fat_headlines = false,
-        bullets = {},
-      },
-    },
-  },
-}
+return {}
