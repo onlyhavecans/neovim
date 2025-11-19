66ec24af024fc34798df1ea4734f3586802b8456
Amelia Aronsohn <squirrel@wearing.black>
2025-11-19 11:12:35 -0800

    fix: tweak defaults

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,11 +22,11 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.dial" },
-    { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.neo-tree" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.util.dot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
