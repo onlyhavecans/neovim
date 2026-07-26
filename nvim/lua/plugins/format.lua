74fc5918ad4ddce4ce089a7d5dc2cb6a0af40dec
Amelia Aronsohn <squirrel@wearing.black>
2026-07-26 08:08:02 -0700

    fix(format): disable autoformat json & yaml

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -66,7 +66,7 @@ return {
 
       format_on_save = function(bufnr)
         -- Disable autoformat for certain filetypes
-        local disable_filetypes = { markdown = true }
+        local disable_filetypes = { markdown = true, yaml = true, json = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         end
